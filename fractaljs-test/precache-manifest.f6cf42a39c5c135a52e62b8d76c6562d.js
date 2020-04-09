ee256c4f7b8181ab3d92b3101ab93cbec38716aa
matthieudumas <matthieu.dumas.extern@axelspringer.com>
2020-04-09 11:45:14 +0200

    new version

---
+++ fractaljs-test/precache-manifest.f6cf42a39c5c135a52e62b8d76c6562d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb377d6ef449916be112434ab5ffc4f9",
+    "url": "./184f355f6bc47b7f0801.worker.js"
+  },
+  {
+    "revision": "d8764495c0e657a6ccdf67e239dce3cd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "206ebf738ab8f980feee",
+    "url": "./static/css/main.53a14f61.chunk.css"
+  },
+  {
+    "revision": "000fc1cc162a7d6a598b",
+    "url": "./static/js/2.81c8e450.chunk.js"
+  },
+  {
+    "revision": "754901f324834d6fb01f271af64e585e",
+    "url": "./static/js/2.81c8e450.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "206ebf738ab8f980feee",
+    "url": "./static/js/main.5b6c76ce.chunk.js"
+  },
+  {
+    "revision": "9475ad40d007a22fed35",
+    "url": "./static/js/runtime-main.0ce4902a.js"
+  }
+]);
--- fractaljs-test/service-worker.js
+++ fractaljs-test/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0fcd3e7eaaaafb81a7ff7381b6fa7685.js"
+  "./precache-manifest.f6cf42a39c5c135a52e62b8d76c6562d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
