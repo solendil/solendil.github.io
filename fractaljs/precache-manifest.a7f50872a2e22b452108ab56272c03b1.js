23b310059f87ad50c6e3d13af4b10038b275e596
matthieudumas <matthieu.dumas.extern@axelspringer.com>
2020-04-10 10:39:34 +0200

    new version

---
+++ fractaljs/precache-manifest.a7f50872a2e22b452108ab56272c03b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb377d6ef449916be112434ab5ffc4f9",
+    "url": "./184f355f6bc47b7f0801.worker.js"
+  },
+  {
+    "revision": "1246f40e9b737871bee00476172b56eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c19a58b9e5e0115650b4",
+    "url": "./static/css/main.53a14f61.chunk.css"
+  },
+  {
+    "revision": "69f66433d94bd03a6ca9",
+    "url": "./static/js/2.b1f3995c.chunk.js"
+  },
+  {
+    "revision": "754901f324834d6fb01f271af64e585e",
+    "url": "./static/js/2.b1f3995c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c19a58b9e5e0115650b4",
+    "url": "./static/js/main.e4a450b9.chunk.js"
+  },
+  {
+    "revision": "9475ad40d007a22fed35",
+    "url": "./static/js/runtime-main.0ce4902a.js"
+  }
+]);
